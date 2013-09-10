fcb193371850e5fdd221afa0b9fa63553e0afc55
yhwh <ferrero.nicolas@gmail.com>
2013-09-10 23:49:15 +0200

    Quiet apt-get update

--- .travis/scripts/install_browsers.sh
+++ .travis/scripts/install_browsers.sh
@@ -11,5 +11,5 @@ sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /et
 wget -q -O - http://deb.opera.com/archive.key | sudo apt-key add -
 sudo sh -c 'echo "deb http://deb.opera.com/opera/ stable non-free" >> /etc/apt/sources.list'
 
-sudo apt-get update --force-yes
+sudo apt-get update -qq --force-yes
 sudo apt-get install -qq --force-yes xvfb imagemagick google-chrome-stable opera
