93e4768f9b60940fb192a144afe92c1604454528
yhwh <ferrero.nicolas@gmail.com>
2013-09-11 17:21:04 +0200

    Tweak continuous integration/deployment scripts

--- .travis/ghpages-deploy.sh
+++ .travis/ghpages-deploy.sh
@@ -2,11 +2,15 @@
 
 # deploy to jsdoc only if the build is run on the master branch
 
-if [ "$TRAVIS_BRANCH" == "master" ] ; then
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
 
     git clone https://$GITHUB_LOGIN:$GITHUB_PASSWORD@github.com/yhwh/domino.git
 
     cd domino
+
+    git config --global user.name "$GIT_USERNAME"
+
+    git config --global user.email $GIT_USEREMAIL
 
     git fetch origin
 
@@ -14,7 +18,7 @@ if [ "$TRAVIS_BRANCH" == "master" ] ; then
 
     git pull origin gh-pages
 
-    cp -r docs .
+    cp -r ../docs .
 
     git add docs
 
@@ -22,9 +26,6 @@ if [ "$TRAVIS_BRANCH" == "master" ] ; then
 
     git push origin gh-pages
 
-    git config --global user.name "$GIT_USERNAME"
-
-    git config --global user.email $GIT_USEREMAIL
 
 fi
 
