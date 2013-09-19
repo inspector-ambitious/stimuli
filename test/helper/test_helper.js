b82818c660fad8225ec113ff8e8ede973a3bc9b9
Nicolas Ferrero <ferrero.nicolas@gmail.com>
2013-09-19 23:54:05 +0200

    feature: finish to implement the mouse event synthesiser

--- test/helper/test_helper.js
+++ test/helper/test_helper.js
@@ -1,18 +1,56 @@
 'use strict';
 
-var TestHelper = {};
 
-// Load a html fixture in the body
-TestHelper.loadTemplate = function(url, callback) {
 
-    if (Stimuli.$('#template').length === 0) {
-        Stimuli.$('body').append('<div id="template"></div>');
-    }
-    
-    Stimuli.$('#template').load(url, callback);
-};
+    var TestHelper = {
 
-// 
-TestHelper.removeTemplate = function() {
-     Stimuli.$('#template').remove();
-};
+        ajax: function(method, url, callback) {
+            var req;
+            try {
+                req = new XMLHttpRequest();
+            } catch (ex) {
+                req = new window.ActiveXObject("Microsoft.XMLHTTP");
+            }
+            req.onload = function() {
+                req.onload = null;
+                callback(this.responseText);
+            };
+            req.onreadystatechange = function() {
+                if(req.readyState === 4){
+                    req.onreadystatechange = null;
+                    callback(this.responseText);
+                }
+            };
+            req.open(method, url, true);
+            req.send();
+        },
+
+        // Load a html fixture in the body
+        loadFixture: function(name, callback) {
+
+            if (!Stimuli.$('#fixtureContainer')) {
+                var fixtureContainer = document.createElement('div');
+                fixtureContainer.id = 'fixtureContainer';
+                Stimuli.$('body').appendChild(fixtureContainer);
+            }
+            
+            TestHelper.ajax('get', 'base/test/fixtures/' + name + '.html', function(response) {
+                Stimuli.$('#fixtureContainer').innerHTML = response;
+                callback();
+            });
+            
+        },
+
+        // remove a fixture
+        removeFixture: function(fixture) {
+            var fixtureContainer = Stimuli.$('#fixtureContainer');
+            if (fixtureContainer) {
+                fixtureContainer.innerHTML = '';
+                if (fixtureContainer.childNodes.length === 0) {
+                    Stimuli.$('body').removeChild(fixtureContainer);
+                    fixtureContainer = null;
+                }
+            }
+        }
+
+    };
