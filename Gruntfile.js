f2ca8e2f63ada936db8f1a6bc3ba9b2ee0efa227
Nicolas Ferrero <ferrero.nicolas@gmail.com>
2013-09-28 13:56:10 +0200

    chore: update travis browser list to use local firefox instead of sauce

--- Gruntfile.js
+++ Gruntfile.js
@@ -138,7 +138,7 @@ module.exports = function(grunt) {
 
                 files: conf.testFilesBuild,
 
-                browsers: ['PhantomJS', 'SL_Chrome', 'SL_Firefox', 'SL_Safari', 'SL_IE8', 'SL_IE9', 'SL_IE10']
+                browsers: ['PhantomJS','Firefox', 'SL_Chrome', 'SL_Safari', 'SL_IE8', 'SL_IE9', 'SL_IE10']
 
             },
 
