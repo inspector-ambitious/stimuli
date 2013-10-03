e9fdc3cb3d05e93201f596b6e50f76d4efa20f19
Nicolas Ferrero <ferrero.nicolas@gmail.com>
2013-10-03 20:40:12 +0200

    fix: anchor click navigation on ie9 and ie10

--- src/mouse/click.js
+++ src/mouse/click.js
@@ -121,6 +121,8 @@
             if (href || hash || form) {
                 // click doesn't fire on the window in ie8 but on the document.
                 var isIE8 = Stimuli.core.Support.isIE8,
+                    isIE9 = Stimuli.core.Support.isIE9,
+                    isIE10 = Stimuli.core.Support.isIE10,
                     win = self.viewport.getWindow(),
                     observer = new Stimuli.event.Observer(isIE8 ? win.document : win);
 
@@ -132,10 +134,10 @@
                         if (hash) {
                             win.location.hash = hash;
                         } else if (href) {
-                            if (!isIE8) {
+                            if (!isIE8 && !isIE9 && !isIE10) {
                                 win.location.href = href;
                             } else {
-                                // ie8 doesn't handle relative href let's forge it
+                                // ie8-10 don't handle relative href passed to window.location let's forge it
                                 var match = win.location.href.match(/[^\/]*$/),
                                     prefix = '';
                                 if (!/:\/\//.test(href)) {
